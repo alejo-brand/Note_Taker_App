9022198e43b2f302ba2df6c4288fbf5f9404880e
alejandro Brand <alejandrobrand8@gmail.com>
2020-12-28 16:33:03 -0800

    post route logging correctly

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/api/notes", function(req,res){
             
         // console.log(data);
         //then parse the file content with json.parse to get the real data (I get an array of objects)
-        const dbNote = JSON.parse(data); 
+        let dbNote = JSON.parse(data); 
         //use send.json to (send the parsed data back to the browser)
         // console.log(dbNote);
         return res.json(dbNote);
@@ -32,11 +32,19 @@ app.post("/api/notes", function(req,res){
     //access the POSTED data
     const newNote = req.body;
     //read file wwith fs module
+    fs.readFile (__dirname + '/db/db.json','utf8',(err,data)=>{
+        if(err) throw  err;
+       let dbNote = JSON.parse(data);
+       dbNote.push(newNote);
+       console.log(dbNote);
+
+       fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(dbNote));
+    })
 })
 
 // this route needs to return the content of notes.html
 app.get("/notes",function(req,res){
-    res.sendFile(path.join(__dirname,"notes.html"))
+    res.sendFile(path.join(__dirname,"/public/notes.html"))
 });
 //this route needs to return the content of index.html
 app.get("*",function(req,res){
